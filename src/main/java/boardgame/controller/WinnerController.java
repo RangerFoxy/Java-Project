3761c1c7193a7c7ca9b84060ba6e327f123d7442
Szabó Dániel <daniszabobtw@gmail.com>
2021-05-15 21:21:49 +0200

    Started victory scene

---
+++ src/main/java/boardgame/controller/WinnerController.java
@@ -0,0 +1,33 @@
+package boardgame.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WinnerController {
+
+    @FXML
+    private Label playerName;
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    public void newAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void exitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void changeLabel() {
+
+    }
+}
